64251cc64b8c21937ba6670a0c5e3665e5beefb1
Abhishek <>
2025-11-30 16:37:33 +0530

    better shortcuts to switch between windows nvim

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -22,4 +22,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
-
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
